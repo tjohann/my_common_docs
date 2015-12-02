7bafb313d8b02268d3d8102d2b117560379e05f4
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2015-12-02 16:39:33 +0100

    add some info about exit() and return add some info + code around message-queue's

---
+++ programming/msg_server/msg_server.c
@@ -0,0 +1,13 @@
+/*
+ * a simple message-queue server example
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	fprintf(stdout, "Hello from %s in %s\n", __FUNCTION__, __FILE__);
+
+	exit(0);
+}
---
+++ programming/return_or_exit.c
@@ -0,0 +1,37 @@
+/*
+ * check the difference of return and exit()
+ *
+ * usage:
+ * gcc -S -O1 return_or_exit.c
+ * cat return_or_exit.s 
+
+ *
+ * result with return (only relevant parts):
+ *
+main:
+.LFB10:
+	.cfi_startproc
+	movl	$0, %eax
+	ret
+	.cfi_endproc
+
+ *
+ * result with exit() (only relevant parts):
+ *
+main:
+.LFB10:
+	.cfi_startproc
+	subq	$8, %rsp
+	.cfi_def_cfa_offset 16
+	movl	$0, %edi
+	call	exit
+	.cfi_endproc
+ */
+
+#include <stdlib.h>   // for exit()
+
+int main(void) {
+
+	//return 0;
+	exit (0);
+}
