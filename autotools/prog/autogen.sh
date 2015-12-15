922e30cc7df984ec2f64f33a17af7cfda84cd806
Thorsten Johannvorderbrueggen <myhalfdan@googlemail.com>
2015-12-15 17:01:03 +0100

    add needed files

--- autotools/prog/autogen.sh
+++ autotools/prog/autogen.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-aclocal \
-&& automake --add-missing \
-&& autoconf
+set -e
+
+if [ -f .git/hooks/pre-commit.sample -a ! -f .git/hooks/pre-commit ] ; then
+        cp -p .git/hooks/pre-commit.sample .git/hooks/pre-commit && \
+        chmod +x .git/hooks/pre-commit && \
+        echo "Activated pre-commit hook."
+fi
+
+autoreconf --install 
